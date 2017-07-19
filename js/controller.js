ee9a8ac7a1edb8ac6e1814e10ce000d82b8797be
vicky <897713389@qq.com>
2017-07-19 15:26:36 +0800

    05-编辑数据完成

--- js/controller.js
+++ js/controller.js
@@ -23,5 +23,17 @@
 
             //--------------03- 删除数据-----------------
             vm.del = TodoSrv.del;
+
+            //--------------04- 修改数据------------------
+            vm.editingId = -1;
+            vm.edit = function(id){
+                //当前编辑的时候,让editingId = id ,值变为 true,就添加 editing 类
+                vm.editingId = id;
+            }
+            //保存修改的数据
+            vm.editSave = function(){
+                 vm.editingId = -1;
+                 TodoSrv.save();
+            }
         }
 })(angular);
