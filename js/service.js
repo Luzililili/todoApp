f75d7a10aecf3fdf2bb77eb60f0bfe0ed4364f93
vicky <897713389@qq.com>
2017-07-19 15:09:32 +0800

    04-删除数据完成

--- js/service.js
+++ js/service.js
@@ -1,35 +1,46 @@
-(function(angular){
-   'use strict';
+(function (angular) {
+    'use strict';
     angular
-    .module('todoApp.service',[])
-    .service('TodoSrv',['$window',function($window){
-        //--------------00- 存储数据---------------
-        //先从localStorage中获取数据
-        var localStorage = $window.localStorage;
-        var todoList = JSON.parse(localStorage.getItem('todo'));
-        //存储数据
-        this.save = function(){
-            localStorage.setItem('todo', JSON.stringify(todoList));
-        };
-        
-        //因为有this的指向问题,所以声明一个变量
-        var that = this;
-        //--------------01- 数据展示----------------
-        this.getData = function(){
-            return todoList;
-        }
-       
-       //--------------02- 添加数据-----------------
-       this.add = function(taskName){
-           var id = todoList.length === 0 ? 1 :　todoList[todoList.length - 1].id + 1;
-           //添加到数组中
-           todoList.push({
-               id:id,name:taskName,isCompleted:false
-           })
-           //添加后保存一下数据
-           this.save();
-       }
+        .module('todoApp.service', [])
+        .service('TodoSrv', ['$window', function ($window) {
+            //--------------00- 存储数据---------------
+            //先从localStorage中获取数据
+            var localStorage = $window.localStorage;
+            var todoList = JSON.parse(localStorage.getItem('todo'));
+            //存储数据
+            this.save = function () {
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            };
 
+            //因为有this的指向问题,所以声明一个变量
+            var that = this;
+            //--------------01- 数据展示----------------
+            this.getData = function () {
+                return todoList;
+            }
 
-    }])
+            //--------------02- 添加数据-----------------
+            this.add = function (taskName) {
+                var id = todoList.length === 0 ? 1 : 　todoList[todoList.length - 1].id + 1;
+                //添加到数组中
+                todoList.push({
+                    id: id, name: taskName, isCompleted: false
+                })
+                //添加后存储一下数据
+                that.save();
+            }
+
+            //--------------03- 删除数据-----------------
+            this.del = function(id){
+                for( var i = 0; i < todoList.length; i++ ){
+                    if( todoList[i].id === id ){
+                        todoList.splice( i , 1 );
+                        break;
+                    }
+                }
+                //删除完后,存储数据
+                 that.save();
+            }
+
+        }])
 })(angular);
